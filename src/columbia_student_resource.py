d4defe73ebbce2063dd2b902190796aa7f25ae32
ZefangWang <91424340+ZefangWang@users.noreply.github.com>
2022-09-25 22:35:49 -0400

    Add files via upload

--- src/columbia_student_resource.py
+++ src/columbia_student_resource.py
@@ -16,9 +16,9 @@ class ColumbiaStudentResource:
         h = os.environ.get("DBHOST")
 
         conn = pymysql.connect(
-            user=usr,
-            password=pw,
-            host=h,
+            user="admin",
+            password="password",
+            host="e6156.c4ybz2geahe7.us-east-1.rds.amazonaws.com",
             cursorclass=pymysql.cursors.DictCursor,
             autocommit=True
         )
@@ -27,7 +27,7 @@ class ColumbiaStudentResource:
     @staticmethod
     def get_by_key(key):
 
-        sql = "SELECT * FROM f22_databases.columbia_students where guid=%s";
+        sql = "SELECT * FROM f22_databases.columbia_students where uni=%s";
         conn = ColumbiaStudentResource._get_connection()
         cur = conn.cursor()
         res = cur.execute(sql, args=key)
